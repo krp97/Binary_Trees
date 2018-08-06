8d1fb4622e7f7481caa84bafe299f31a55529410
Krystian Plata <k.plata576@gmail.com>
2018-08-06 11:37:14 +0200

    Final BST implementation.

--- Data_Structures/Data_Structures/include/Bst.h
+++ Data_Structures/Data_Structures/include/Bst.h
@@ -7,7 +7,10 @@ class Bst
 public:
 	
 	Bst() : root_{ nullptr } {};
+
 	Bst(Bst_Node* root) : root_{ root } {};
+
+	Bst& operator=(Bst& rhs) { this->root_ = rhs.root_; };
 
 	virtual ~Bst();
 
@@ -15,24 +18,28 @@ public:
 
 	virtual void insert(int value);
 
-	virtual void popRoot();
+	virtual bool popRoot();
 
 	void remove(int value);
 
-	std::string inorder(Bst_Node* node, std::string& output);
+	void inorder(Bst_Node* node, std::string& output);
 
-	std::string preorder(Bst_Node* node, std::string& output);
+	void preorder(Bst_Node* node, std::string& output);
 	
-	std::string postorder(Bst_Node* node, std::string& output);
+	void postorder(Bst_Node* node, std::string& output);
 
-	Bst_Node* get_root() { return root_; };
+	Bst_Node* get_root() const { return root_; };
+
+	bool load_from_file(const std::string& filepath);
+	
+	bool save_to_file(const std::string& filepath);
 
 protected:
 	Bst_Node* root_;
 
 private:
 
-	Bst_Node * find_insert_spot(int value, Bst_Node* node) const;
+	Bst_Node* find_insert_spot(int value, Bst_Node* node) const;
 
 	void remove(Bst_Node* node);
 
@@ -45,11 +52,13 @@ private:
 	void remove_with_children(Bst_Node* node);	// -> Node has both children.
 	// ---------------------------------------------------------------------------
 
-	Bst_Node* find_value(int value);
+	Bst_Node* find_value(int value) const;
 
 	Bst_Node* find_min(Bst_Node* node) const;
 
 	Bst_Node* find_max(Bst_Node* node) const;
 
-	Bst_Node* find_successor(Bst_Node* node);
+	Bst_Node* find_successor(Bst_Node* node) const;
+
+	void prep_for_saving(std::string& toSave);
 };
--- Data_Structures/Data_Structures/src/Bst.cpp
+++ Data_Structures/Data_Structures/src/Bst.cpp
@@ -1,15 +1,18 @@
 #include "stdafx.h"
 #include "..\include\Bst.h"
+#include <fstream>
+#include <algorithm>
 
 Bst::~Bst()
 {
-
+	while (popRoot());
 }
 
 std::string Bst::to_string()
 {
 	auto output = std::string();
-	return inorder(root_, output);
+	inorder(root_, output);
+	return output;
 }
 
 void Bst::insert(int value)
@@ -27,13 +30,33 @@ void Bst::insert(int value)
 		leaf->right_ = new Bst_Node(value, leaf, nullptr, nullptr);
 	else
 		leaf->left_ = new Bst_Node(value, leaf, nullptr, nullptr);
-	
 }
 
-void Bst::popRoot()
+Bst_Node* Bst::find_insert_spot(int value, Bst_Node* node) const
 {
-	if(root_)
+	auto tmp{ node };
+
+	while (tmp)
+	{
+		if (value < tmp->value_ && tmp->left_)
+			tmp = tmp->left_;
+		else if (value >= tmp->value_ && tmp->right_)
+			tmp = tmp->right_;
+		else
+			return tmp;
+	}
+	return nullptr;
+}
+
+bool Bst::popRoot()
+{
+	if (root_)
+	{
 		remove(root_);
+		return true;
+	}
+	else
+		return false;
 }
 
 void Bst::remove(int value)
@@ -42,55 +65,7 @@ void Bst::remove(int value)
 		remove(find_value(value));
 }
 
-std::string Bst::inorder(Bst_Node* node, std::string& output)
-{
-	if (!node)
-		return output;
-
-	inorder(node->left_, output);
-	output += ", " + node->to_string();
-	inorder(node->right_, output);
-
-	return output;
-}
-
-std::string Bst::preorder(Bst_Node* node, std::string& output)
-{
-	if (!node)
-		return std::string();
-
-	output += ", " + node->to_string();
-	preorder(node->left_, output);
-	preorder(node->right_, output);
-
-	return output;
-}
-
-std::string Bst::postorder(Bst_Node* node, std::string& output)
-{
-	if (!node)
-		return std::string();
-
-	postorder(node->left_, output);
-	postorder(node->right_, output);
-	output += ", " + node->to_string();
-
-	return output;
-}
-
-Bst_Node * Bst::find_insert_spot(int value, Bst_Node* node) const
-{
-	if (value < node->value_ && node->left_)
-		return find_insert_spot(value, node->left_);
-
-	else if (node->right_)
-		return find_insert_spot(value, node->right_);
-
-	else
-		return node;
-}
-
-void Bst::remove(Bst_Node * node)
+void Bst::remove(Bst_Node* node)
 {
 	if (node)
 	{
@@ -103,7 +78,7 @@ void Bst::remove(Bst_Node * node)
 	}
 }
 
-void Bst::remove_leaf(Bst_Node * node)
+void Bst::remove_leaf(Bst_Node* node)
 {
 	if (root_ != node)
 	{
@@ -123,7 +98,7 @@ void Bst::remove_leaf(Bst_Node * node)
 	}
 }
 
-void Bst::remove_single_child(Bst_Node * node)
+void Bst::remove_single_child(Bst_Node* node)
 {
 	auto child{ node->right_ ? node->right_ : node->left_ };
 
@@ -131,23 +106,37 @@ void Bst::remove_single_child(Bst_Node * node)
 	delete child;
 }
 
-void Bst::remove_with_children(Bst_Node * node)
+void Bst::remove_with_children(Bst_Node* node)
 {
 	// Find the successor and do the following:
 	// 1 - Copy value of the successor into the node.
 	// 2 - Copy contents of right child of the successor into the successor
 	// 3 - Remove the right child of the successor.
+	// Note that the successor never has a left child.
 
 	auto successor = find_successor(node);
 
 	node->value_ = successor->value_;
 
 	auto right_succ_child = successor->right_;
-	*successor = *right_succ_child;
-	delete right_succ_child;
+	if (right_succ_child)
+	{
+		*successor = *right_succ_child;
+		delete right_succ_child;
+	}
+	else
+	{
+		auto succ_parent{ successor->up_ };
+		if (succ_parent->value_ > successor->value_)
+			succ_parent->left_ = nullptr;
+		else
+			succ_parent->right_ = nullptr;
+		delete successor;
+	}
+		
 }
 
-Bst_Node * Bst::find_value(int value)
+Bst_Node* Bst::find_value(int value) const
 {
 	auto tmp_node {root_};
 
@@ -163,28 +152,28 @@ Bst_Node * Bst::find_value(int value)
 	return tmp_node;
 }
 
-Bst_Node * Bst::find_min(Bst_Node * node) const
+Bst_Node* Bst::find_min(Bst_Node* node) const
 {
 	auto tmp{ node };
 
-	while (tmp)
+	while (tmp->left_)
 		tmp = tmp->left_;
 
 	return tmp;
 
 }
 
-Bst_Node * Bst::find_max(Bst_Node * node) const
+Bst_Node* Bst::find_max(Bst_Node* node) const
 {
 	auto tmp{ node };
 
-	while (tmp)
+	while (tmp->right_)
 		tmp = tmp->right_;
 
 	return tmp;
 }
 
-Bst_Node * Bst::find_successor(Bst_Node * node)
+Bst_Node* Bst::find_successor(Bst_Node* node) const
 {
 	if (node->right_)
 		return find_min(node->right_);
@@ -205,3 +194,102 @@ Bst_Node * Bst::find_successor(Bst_Node * node)
 	}
 }
 
+void Bst::inorder(Bst_Node* node, std::string& output)
+{
+	if (node)
+	{
+		inorder(node->left_, output);
+		output += ", " + node->to_string();
+		inorder(node->right_, output);
+
+		if (output.at(0) == ',')
+			output = output.substr(1);
+	}
+
+}
+
+void Bst::preorder(Bst_Node* node, std::string& output)
+{
+	if (node)
+	{
+		output += ", " + node->to_string();
+		preorder(node->left_, output);
+		preorder(node->right_, output);
+		
+		if (output.at(0) == ',')
+			output = output.substr(1);
+	}
+}
+
+void Bst::postorder(Bst_Node* node, std::string& output)
+{
+	if (node)
+	{
+		postorder(node->left_, output);
+		postorder(node->right_, output);
+		output += ", " + node->to_string();
+
+		if (output.at(0) == ',')
+			output = output.substr(1);
+	}
+
+}
+
+bool Bst::load_from_file(const std::string& filepath)
+{
+	std::ifstream input_file;
+	input_file.open(filepath);
+	
+	auto line{ std::string() };
+	auto value{ 0 };
+
+	if (input_file.is_open())
+	{
+		while (!input_file.eof())
+		{
+			input_file >> line;
+			try
+			{
+				value = stoi(line);
+			}
+			catch (std::invalid_argument)
+			{
+				input_file.close();
+				throw;
+			}
+			catch (std::out_of_range)
+			{
+				input_file.close();
+				throw;
+			}
+
+			insert(value);
+		}
+		return true;
+	}
+	return false;
+}
+
+bool Bst::save_to_file(const std::string& filepath)
+{
+	auto bst_output{ std::string() };
+	preorder(root_, bst_output);
+	prep_for_saving(bst_output);
+
+	std::ofstream output_file;
+	output_file.open(filepath);
+
+	if (output_file.is_open())
+	{
+		output_file << bst_output;
+		output_file.close();
+		return true;
+	}
+	return false;
+}
+
+void Bst::prep_for_saving(std::string& toSave)
+{
+	toSave.erase(std::remove(toSave.begin(), toSave.end(), ' '), toSave.end());
+	toSave.replace(toSave.begin(), toSave.end(), ',', '\n');
+}
